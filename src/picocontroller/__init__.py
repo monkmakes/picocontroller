71879ceaea0b8eeb3928d8ec401625ddb382494e
simonmonk <srmonk@gmail.com>
2025-03-11 10:57:50 +0000

    try again

--- src/picocontroller/__init__.py
+++ src/picocontroller/__init__.py
@@ -1,2 +1,2 @@
-from picocontroller.picocontroller.py import Relay, Buzzer, Button
-from picocontrollergui.picocontrollergui.py import SevenSegDigit, SevenSegDisplay, OLEDConsole, Menu
+from picocontroller.picocontroller import Relay, Buzzer, Button
+from picocontrollergui.picocontrollergui import SevenSegDigit, SevenSegDisplay, OLEDConsole, Menu
